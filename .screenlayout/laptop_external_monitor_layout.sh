3b46cd621b2c36d0ee6829e3142b16ae53007f2a
joseluisgomes <joslus.luis@gmail.com>
2024-09-10 18:11:18 +0100

    Laptop & monitor layout conf. file

---
+++ .screenlayout/laptop_external_monitor_layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --primary --mode 1920x1080 --rate 119.88 --pos 0x0 --rotate normal --output eDP-1-1 --mode 1920x1080 --pos 0x1080 --rotate normal
